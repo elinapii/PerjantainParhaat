ef674a79f4da150ac9b7a08567b04eeb513b6891
Elina Piiroinen <elina.piiroinen@awacademyconsultant.fi>
2021-03-05 16:32:29 +0200

    Kyselyn siistiminen

--- MyyntiDb.sql
+++ MyyntiDb.sql
@@ -2,11 +2,8 @@
 USE MyyntiDB;
 GO
 
-<<<<<<< HEAD
-
 SELECT etunimi, sukunimi FROM Asiakas
 
-=======
 -- Haetaan tilausten summa:
 >>>>>>> 0194c6c58c33042f9e04ee6c7829c192e2d18515
 SELECT SUM(ahinta) FROM TilausRivi;
@@ -18,10 +15,9 @@ go
 
 SELECT etunimi, sukunimi FROM Asiakas;
 
-<<<<<<< HEAD
+
 --Yhdistetään Asiakas ja Tilaus -taulut
 SELECT * FROM Asiakas
 	LEFT JOIN Tilaus
 		ON Asiakas.asiakas_id = Tilaus.asiakas_id;
-=======
->>>>>>> 0194c6c58c33042f9e04ee6c7829c192e2d18515
+
