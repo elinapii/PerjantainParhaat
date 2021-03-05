162868d3394c57752244d1e08c76d36d73ac7bcb
joonasen <80101935+joonasen@users.noreply.github.com>
2021-03-05 16:38:45 +0200

    Update MyyntiDb.sql
    Yksi rivi jäänyt

--- MyyntiDb.sql
+++ MyyntiDb.sql
@@ -1,11 +1,10 @@
--- Käytetään MyyntiDB-kantaa:
+-- KÃ¤ytetÃ¤Ã¤n MyyntiDB-kantaa:
 USE MyyntiDB;
 GO
 
 SELECT etunimi, sukunimi FROM Asiakas
 
 -- Haetaan tilausten summa:
->>>>>>> 0194c6c58c33042f9e04ee6c7829c192e2d18515
 SELECT SUM(ahinta) FROM TilausRivi;
 
 SELECT TOP 10 postitoimipaikka, COUNT(*) FROM Asiakas
@@ -16,7 +15,7 @@ go
 SELECT etunimi, sukunimi FROM Asiakas;
 
 
---Yhdistet��n Asiakas ja Tilaus -taulut
+--Yhdistetään Asiakas ja Tilaus -taulut
 SELECT * FROM Asiakas
 	LEFT JOIN Tilaus
 		ON Asiakas.asiakas_id = Tilaus.asiakas_id;
